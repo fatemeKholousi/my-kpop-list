412fea0778a2b68e8e006eeca2f3fe1e33a00ddc
fateme kholousi <fatemekholousizaer@gmail.com>
2022-06-23 00:48:35 +0430

    fix : encrypting password added

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { validationResult,check } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { validationResult, check } = require('express-validator')
 const router = express.Router()
-
 const User = require('../models/User')
 
 // @route           POST api/users
@@ -17,13 +18,35 @@ router.post(
       'Please enter a valid password with 6 or more charachters',
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' })
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      })
+
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+   
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).send('server error')
+    }
   },
 )
 
