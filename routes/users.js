3319240e7cb4738786ce3467a0fbab59ace53824
fateme kholousi <fatemekholousizaer@gmail.com>
2022-06-22 22:43:13 +0430

    feat: models added

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
-const express=require("express")
-const router=express.Router()
+const express = require('express')
+const { validationResult,check } = require('express-validator')
+const router = express.Router()
+
+const User = require('../models/User')
 
 // @route           POST api/users
-// @desc(ription)   Register a user  
+// @desc(ription)   Register a user
 // @access          public
-router.post('/',(req,res)=>{
-    res.send("Register a user")
-})
+router.post(
+  '/',
+  [
+    check('name', 'Please enter name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a valid password with 6 or more charachters',
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
 
-module.exports=router
+    res.send('passed')
+  },
+)
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app=express()
 //  connect database
 connectDB()
 
+//  init middleware
+app.use(express.json({extended:false}))
 
 app.get('/',(req,res)=>{res.json({msg:"Welcome to my world my show!"})})
 
